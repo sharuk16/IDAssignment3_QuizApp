529a1e61be60de65ab8749ea78a00c67b66f748b
sharuk16 <73157970+sharuk16@users.noreply.github.com>
2021-02-22 22:47:19 +0800

    Update game.js

--- game.js
+++ game.js
@@ -64,7 +64,7 @@ startGame = () => {
 getNewQuestion = () => {
   if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("highScore", score);
-    return window.location.assign("/end.html");
+    return window.location.assign("end.html");
   }
   questionCounter++;
   questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS
